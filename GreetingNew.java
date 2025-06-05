35940e52bed709fd22104be6958432d2bf0bca79
jhbyun97 <jhbyun97@psm.hanyang.ac.kr>
2025-06-05 19:06:09 +0900

    message

--- GreetingNew.java
+++ GreetingNew.java
@@ -8,5 +8,6 @@ public class GreetingNew
     {
         System.out.println("Hello BBBBB GoGoGo!");
         System.out.println("Hello BBBBB GoGoGo!");
+        System.out.println("Hello BBBBB GoGoGo!");
     }
 }
