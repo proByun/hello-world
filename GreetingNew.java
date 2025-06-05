812df67be050e974bf4723f41ce55b3b674a2718
Junhyun Byun <60165706+proByun@users.noreply.github.com>
2025-06-05 19:43:23 +0900

    Update GreetingNew.java

--- GreetingNew.java
+++ GreetingNew.java
@@ -6,7 +6,7 @@ public class GreetingNew
     // Prints a greeting message.
     public static void main(String[] args)
     {
-        System.out.println("Hello BBBBB GoGoGo!");
+        System.out.println("Hello!");
         System.out.println("Hello BBBBB GoGoGo!");
         System.out.println("Hello BBBBB GoGoGo!");
 System.out.println("Hello BBBBB GoGoGo!");
